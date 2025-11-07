5d7f62ff5dd30b500d52a4a24dd90185135ea54e
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-11-07 21:42:52 +0100

    first commit

--- src/main/java/com/menstrualtrackerapplication/service/CycleCalculatorService.java
+++ src/main/java/com/menstrualtrackerapplication/service/CycleCalculatorService.java
@@ -1,8 +1,13 @@
 package service;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.Map;
 
+@Service
 public class CycleCalculatorService {
-    public Map<String, LocalDate> calculateCycle(LocalDate last)
+    public Map<String, LocalDate> calculateCycle(LocalDate lastPeriodStart, int cycleLength, int periodDuration) {
+        Map<String, LocalDate> result = new HashMap<>();
+    }
+
 }
