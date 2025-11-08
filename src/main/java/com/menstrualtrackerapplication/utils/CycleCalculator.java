134e99e6e58412d6677eb5b83ed79d4d3e1a68d5
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-11-08 22:21:42 +0100

    first commit

--- src/main/java/com/menstrualtrackerapplication/utils/CycleCalculator.java
+++ src/main/java/com/menstrualtrackerapplication/utils/CycleCalculator.java
@@ -1,4 +1,17 @@
 package utils;
 
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDate;
+
 public class CycleCalculator {
+    public static LocalDate addDays(LocalDate date, int days){
+        return date.plusDays(days);
+    }
+    public static LocalDate subtractDays(LocalDate date, int days){
+        return date.minusDays(days);
+    }
+    public static CycleDates calculateCycle(LocalDate start, int cycleLength, int duration){
+        LocalDate period
+    }
 }
