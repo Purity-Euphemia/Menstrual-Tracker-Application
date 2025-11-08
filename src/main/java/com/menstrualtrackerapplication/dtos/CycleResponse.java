f327c1bd2401955d0288c2c2cfcc33c430b69cfa
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-11-08 21:59:30 +0100

    first commit

--- src/main/java/com/menstrualtrackerapplication/dtos/CycleResponse.java
+++ src/main/java/com/menstrualtrackerapplication/dtos/CycleResponse.java
@@ -1,4 +1,19 @@
 package dtos;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
 public class CycleResponse {
+    private LocalDate periodStart;
+    private LocalDate periodEnd;
+    private LocalDate nextPeriodStart;
+    private LocalDate ovulationDay;
+    private LocalDate fertileStart;
+    private LocalDate fertileEnd;
+    private LocalDate safeStart;
+    private LocalDate safeEnd;
 }
