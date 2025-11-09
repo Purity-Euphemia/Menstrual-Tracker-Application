009784d00555ac5e972ce683356960f5b705b257
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-11-09 21:47:24 +0100

    first commit

--- src/main/java/com/menstrualtrackerapplication/service/CycleService.java
+++ src/main/java/com/menstrualtrackerapplication/service/CycleService.java
@@ -3,6 +3,7 @@ package service;
 import dtos.CycleRequest;
 import dtos.CycleResponse;
 import lombok.RequiredArgsConstructor;
+import model.UserCycle;
 import org.springframework.stereotype.Service;
 import repository.UserCycleRepository;
 import utils.CycleCalculator;
@@ -22,7 +23,21 @@ public class CycleService {
 
         var calc = CycleCalculator.calculateCycle(start, request.getCycleLength(), request.getPeriodDuration());
 
-        
+        UserCycle cycle = UserCycle.builder()
+                .lastPeriodStart(calc.periodStart())
+                .cycleLength(request.getCycleLength())
+                .periodDuration(request.getPeriodDuration())
+                .nextPeriodStart(calc.nextPeriodStart())
+                .periodEnd(calc.periodEnd())
+                .ovulationDay(calc.ovulationDay())
+                .fertileStart(calc.fertileStart())
+                .fertileEnd(calc.fertileEnd())
+                .safeStart(calc.safeStart())
+                .safeEnd(calc.safeEnd())
+                .build();
+
+        userCycleRepository.save(cycle);
+
 
 
 
