a4e8100fbaa3753649aab4eb94b5e8dbfbf45aa3
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-11-09 21:50:12 +0100

    first commit

--- src/main/java/com/menstrualtrackerapplication/controller/CycleController.java
+++ src/main/java/com/menstrualtrackerapplication/controller/CycleController.java
@@ -1,4 +1,20 @@
 package controller;
 
+import dtos.CycleRequest;
+import dtos.CycleResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import service.CycleService;
+
+@RestController
+@RequestMapping("/api/cycle")
+@CrossOrigin(origins = "http://localhost:3000")
+@RequiredArgsConstructor
 public class CycleController {
+    private final CycleService cycleService;
+
+    @PostMapping("/calculate")
+    public CycleResponse calculateCycle(@RequestBody CycleRequest request) {
+        return cycleService.calculateAndSaveCycle(request);
+    }
 }
